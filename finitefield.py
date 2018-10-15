4d43ac3833db1456c515505706a7908e3ba5f682
此Repo仅供测试用 <xidalapuda@126.com>
2018-10-15 16:38:21 +0800

    Update finitefield.py

--- finitefield.py
+++ finitefield.py
@@ -1,1 +1,9 @@
 def divide_and_remainer(n, p):
+    """Divide a polynomail `n` by `p`."""
+    q = 0
+    plen = p.bit_length() - 1
+    while n.bit_length() >= plen:
+        d = n.bit_length() - plen
+        n ^= (p << d)
+        q ^= (1 << i)
+    return q, n
